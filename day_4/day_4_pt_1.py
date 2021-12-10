6405d955b2e1e73e0226993637ba865ff9bf5dcb
David Henry <77511476+aG00Dtime@users.noreply.github.com>
2021-12-10 12:20:58 -0400

    Part 1 done

--- day_4/day_4_pt_1.py
+++ day_4/day_4_pt_1.py
@@ -1,6 +1,9 @@
+import copy
+import numpy as np
+# temp lists
 boards = []
 temp_list = []
-
+# open file and get rows
 with open("input.txt", "r") as file:
     sequence = file.readline()
     sequence = list(map(int, sequence.replace(",", " ").split()))
@@ -11,13 +14,28 @@ with open("input.txt", "r") as file:
                 row = file.readline().split()
                 row = list(map(int, row))
                 temp_list.append(row)
-        boards.append(temp_list[:])
+        boards.append(temp_list.copy())
+
         temp_list.clear()
+# copy board list
+board_copy = copy.deepcopy(boards)
 
-winning_board = None
+
+def calc_sum(array, num):
+    # print final score of board
+    # smaller number wins
+    b_sum = 0
+    for row_ in array:
+        for val in row_:
+            if val != "x":
+                b_sum += val
+    print(f"Total score is {b_sum * num}")
+
+
+board_1 = None
 last_num = None
-board_sum = 0
-
+# should make this a function but w.e
+# check rows to see if numbers match sequence
 for num in sequence:
     for board in boards:
         # rows
@@ -25,16 +43,34 @@ for num in sequence:
             if num in row:
                 row[row.index(num)] = "x"
                 last_num = num
+            if not board_1:
                 if row == ["x"] * len(row):
-                    winning_board = board
-                    for i in range(5):
-                        print(board[i])
+                    board_1 = board
+                    # print(board)
+                    calc_sum(board_1, last_num)
 
-                    for rows in winning_board:
-                        for nums in rows:
-                            if nums != "x":
-                                board_sum += nums
+# make numpy array to turn columns into rows
+boards_deepcopy = np.array(board_copy)
+board_cols = []
+board_2 = None
+# turn columns into rows
+for board in boards_deepcopy:
+    for i in range(5):
+        temp_list.append([row[i] for row in board])
+    board_cols.append(temp_list[:])
+    temp_list.clear()
 
-                    print(f"Last number is {last_num} and the sum of the board is {board_sum}")
-                    print(f"Final score is {board_sum * last_num}")
-                    exit()
+for num in sequence:
+    for board in board_cols:
+        # rows
+        for value, row in enumerate(board):
+            if num in row:
+                row[row.index(num)] = "x"
+                last_num = num
+            if not board_2:
+                if row == ["x"] * len(row):
+                    board_2 = board
+                    # print(board)
+                    calc_sum(board_2, last_num)
+# Total score is 35056
+# Total score is 4662
